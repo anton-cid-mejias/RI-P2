4db08fcde777144c600475d6d32c8447bdb7ca8b
anton-cid-mejias <anton.cid.mejias@gmail.com>
2017-02-20 19:44:20 +0100

    Created initial project

---
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/App.java
@@ -0,0 +1,13 @@
+package es.udc.fic.mri_indexer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
