cda26019dddd64bc8e275448545fa10154149d28
pedro-rivero <pedrorivero19@gmail.com>
2017-04-25 16:56:54 +0200

    Vitas

--- mri-searcher/src/main/java/es/udc/fic/mri_searcher/IndexerUI.java
+++ mri-searcher/src/main/java/es/udc/fic/mri_searcher/IndexerUI.java
@@ -3,21 +3,63 @@ package es.udc.fic.mri_searcher;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 
 public class IndexerUI {
-    
+
     public static void main(String[] args) {
-	/*
-	 * 	-openmode openmode (el open mode será append, create, o create_or_append)
-		-index pathname (ruta de la carpeta que contiene o contendrá el índice)
-		-indexingmodel default | jm lambda | dir mu (indica el modelo de RI en la creación del índice)
-		-coll pathname (ruta de la carpeta que contiene los archivos  de la colección)
-	 */
-	
-	
-	
+
+	if ((args.length == 0) || (args.length > 0
+		&& ("-h".equals(args[0]) || "-help".equals(args[0])))) {
+	    print_usage_and_exit();
+	}
+
 	String openmodeString = null;
 	OpenMode openmode = OpenMode.CREATE_OR_APPEND;
+	String index = null;
+	String coll = null;
+	// el indexingmodel de momento no sé qué hacer con él
+
+	for (int i = 0; i < args.length; i++) {
+	    if ("-openmode".equals(args[i])) {
+		openmodeString = args[i + 1];
+		if ((!openmodeString.equals("append"))
+			&& (!openmodeString.equals("create"))
+			&& (!openmodeString.equals("create_or_append"))) {
+		    System.err.println(
+			    "Openmode must be: append, create or append_or_create.");
+		    System.exit(-1);
+		}
+		i++;
+	    } else if ("-index".equals(args[i])) {
+		index = args[i + 1];
+		i++;
+	    } else if ("-coll".equals(args[i])) {
+		coll = args[i + 1];
+		i++;
+	    }
+	}
 	
+	if ((index == null) || (coll == null)){
+	    System.out.println("Options -index and -coll are required to run this program");
+	}
+
+	// Openmode will be create_or_append by default
+	if (openmodeString != null) {
+	    if (openmodeString.equals("create")) {
+		openmode = OpenMode.CREATE;
+	    } else if (openmodeString.equals("append")) {
+		openmode = OpenMode.APPEND;
+	    }
+	}
+
 	
+	//Aquí el if gigante para llamar al indexer
+
+    }
+
+    private static void print_usage_and_exit() {
+	String usage = "Usage: Indexing options [-openmode openmode] [-index pathname] [-coll pathname] "
+		+ "[-indexingmodel default| jm lambda | dir mu] \n";
+	System.out.println(usage);
+	System.exit(0);
     }
 
 }
