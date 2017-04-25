864d64f5a5910bec10947281c19da74d3e85a846
pedro-rivero <pedrorivero19@gmail.com>
2017-04-25 16:47:21 +0200

    Pom xml bitch

---
+++ mri-searcher/src/main/java/es/udc/fic/mri_searcher/IndexerUI.java
@@ -0,0 +1,23 @@
+package es.udc.fic.mri_searcher;
+
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+
+public class IndexerUI {
+    
+    public static void main(String[] args) {
+	/*
+	 * 	-openmode openmode (el open mode será append, create, o create_or_append)
+		-index pathname (ruta de la carpeta que contiene o contendrá el índice)
+		-indexingmodel default | jm lambda | dir mu (indica el modelo de RI en la creación del índice)
+		-coll pathname (ruta de la carpeta que contiene los archivos  de la colección)
+	 */
+	
+	
+	
+	String openmodeString = null;
+	OpenMode openmode = OpenMode.CREATE_OR_APPEND;
+	
+	
+    }
+
+}
