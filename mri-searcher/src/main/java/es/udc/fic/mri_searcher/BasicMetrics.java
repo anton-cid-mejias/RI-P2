b30417b25c7d787397e188a775f033ae98663ec9
anton-cid-mejias <anton.cid.mejias@gmail.com>
2017-04-29 19:52:20 +0200

    Remodeled BasicMetrics

--- mri-searcher/src/main/java/es/udc/fic/mri_searcher/BasicMetrics.java
+++ mri-searcher/src/main/java/es/udc/fic/mri_searcher/BasicMetrics.java
@@ -5,26 +5,7 @@ import java.util.List;
 
 public class BasicMetrics {
 
-    public static float[] Precision(RelevantDocumentsAndHits rel) {
-	
-	float[] p = new float[2];
-	p[0] = rel.getHits10()/10;
-	p[1] = rel.getHits20()/20;
-	
-	return p;
-	
-    }
-
-    public static float[] Recall(RelevantDocumentsAndHits rel) {
-
-	float[] recall = new float[2];
-	recall[0] = rel.getHits10()/rel.getTotalRelevants();
-	recall[1] = rel.getHits20()/rel.getTotalRelevants();
-	
-	return recall;
-    }
-
-    public static RelevantDocumentsAndHits RelevanceHits(int queryNumber,
+    public static RelevantDocumentsAndMetrics RelevanceHits(int queryNumber,
 	    List<Integer> queryDocs, List<QueryNumberRelevanceDoc> cranRel) {
 	
 	List<Integer> relevanceDocs = null;
@@ -56,6 +37,14 @@ public class BasicMetrics {
 	    }
 	}
 	
-	return new RelevantDocumentsAndHits(hits10,hits20,relevanceDocsHits,relevanceDocs.size());
+	float[] p = new float[2];
+	p[0] = hits10/10;
+	p[1] = hits20/20;
+	
+	float[] recall = new float[2];
+	recall[0] = hits10/relevanceDocs.size();
+	recall[1] = hits20/relevanceDocs.size();
+	
+	return new RelevantDocumentsAndMetrics(hits10,hits20,relevanceDocsHits,relevanceDocs.size(),p,recall,0);
     }
 }
---
+++ mri-searcher/src/main/java/es/udc/fic/mri_searcher/RelevantDocumentsAndMetrics.java
@@ -0,0 +1,51 @@
+package es.udc.fic.mri_searcher;
+
+import java.util.List;
+
+public class RelevantDocumentsAndMetrics {
+    
+    private final int hits10;
+    private final int hits20;
+    private final List<Integer> relevantDocs;
+    private final int totalRelevants;
+    private final float[] precision;
+    private final float[] recall;
+    private final float averagePrecision;
+    
+    public RelevantDocumentsAndMetrics(int hits10, int hits20,
+	    List<Integer> relevantDocs, int totalRelevants, float[] precision,
+	    float[] recall, float averagePrecision) {
+	super();
+	this.hits10 = hits10;
+	this.hits20 = hits20;
+	this.relevantDocs = relevantDocs;
+	this.totalRelevants = totalRelevants;
+	this.precision = precision;
+	this.recall = recall;
+	this.averagePrecision = averagePrecision;
+    }
+    
+    public int getHits10() {
+        return hits10;
+    }
+    public int getHits20() {
+        return hits20;
+    }
+    public List<Integer> getRelevantDocs() {
+        return relevantDocs;
+    }
+    public int getTotalRelevants() {
+        return totalRelevants;
+    }
+    public float[] getPrecision() {
+        return precision;
+    }
+    public float[] getRecall() {
+        return recall;
+    }
+    public float getAveragePrecision() {
+        return averagePrecision;
+    }
+    
+    
+}
