b8335b4d3231919323e49e9a825eb63a121b017d
pedro-rivero <pedrorivero19@gmail.com>
2017-04-29 19:23:54 +0200

    damn son

--- mri-searcher/src/main/java/es/udc/fic/mri_searcher/Searcher.java
+++ mri-searcher/src/main/java/es/udc/fic/mri_searcher/Searcher.java
@@ -12,6 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -19,7 +20,6 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -30,6 +30,7 @@ public class Searcher {
 	    int int2, int cut, int top, String[] fieldsProcs,
 	    String[] fieldsVisual) throws IOException, ParseException {
 
+	List<String> fieldsVisualList = Arrays.asList(fieldsVisual);
 	Directory indir = FSDirectory.open(Paths.get(index));
 	DirectoryReader reader = DirectoryReader.open(indir);
 	IndexSearcher searcher = new IndexSearcher(reader);
@@ -69,9 +70,11 @@ public class Searcher {
 	int actualQueryIndex = Integer.parseInt(actualQuery.get(0));
 	Query query = null;
 	TopDocs topDocs = null;
+	Document doc = null;
 	int hits = 0;
 	List<ScoreDoc> scoreDocs = null;
 	List<Integer> queryDocs = null;
+	RelevantDocumentsAndHits relDocsandHits = null;
 	for (int i = int1; i <= int2; i++) {
 	    if (i == actualQueryIndex) {
 		query = parser.parse(actualQuery.get(1));
@@ -83,7 +86,7 @@ public class Searcher {
 		for (ScoreDoc scoreDoc : scoreDocs){
 		    queryDocs.add(scoreDoc.doc);
 		}
-		BasicMetrics.RelevanceHits(actualQueryIndex,
+		relDocsandHits =BasicMetrics.RelevanceHits(actualQueryIndex,
 			queryDocs, queriesRelevance);
 		
 		/*Imprimir con una función:
@@ -94,7 +97,19 @@ public class Searcher {
 		 * relevancia, y las métricas individuales para cada query.
 		 */
 		
-		//le paso query.toString, reader, scoreDocs, fieldsVisual, y las métricas
+		System.out.println("Query: " + query.toString());
+		for (ScoreDoc scoreDoc : scoreDocs){
+		    doc = reader.document(scoreDoc.doc);
+		    System.out.println("Document: ");
+		    for (String field: fieldsVisualList){
+			System.out.println(field + ": "+ doc.get(field));
+		    }
+		    System.out.println("Score: " + scoreDoc.score);
+		    //marca de relevancia
+		    //métricas llamando a lo de Antón
+		}
+		System.out.println();
+		
 
 		actualQuery = itr.next();
 		actualQueryIndex = Integer.parseInt(actualQuery.get(0));
