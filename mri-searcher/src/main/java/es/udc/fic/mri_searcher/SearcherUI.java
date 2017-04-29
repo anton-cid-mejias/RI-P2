081da0641bd0d61b5e2b70a6472c048086e82fa0
anton-cid-mejias <anton.cid.mejias@gmail.com>
2017-04-29 17:49:45 +0200

    Searcher UI ended

--- mri-searcher/src/main/java/es/udc/fic/mri_searcher/SearcherUI.java
+++ mri-searcher/src/main/java/es/udc/fic/mri_searcher/SearcherUI.java
@@ -19,7 +19,7 @@ public class SearcherUI {
 	int counter = 0;
 	String[] fieldsproc = new String[2];
 	String[] fieldsvisual = new String[5];
-	
+
 	// indexingModel : Null = default, true = jm lambda, false = dir mu
 	Boolean indexingModel = null;
 
@@ -47,34 +47,71 @@ public class SearcherUI {
 		if ("all".equals(args[i + 1])) {
 		    i++;
 		} else {
-		    if (args[i+2].contains("-")){
-			ints = args[i+2].split("-");
+		    if (args[i + 2].contains("-")) {
+			ints = args[i + 2].split("-");
 			int1 = Integer.parseInt(ints[0]);
 			int2 = Integer.parseInt(ints[1]);
-		    }else{
-			int1 = Integer.parseInt(args[i+2]);
+			if (int1 < 0) {
+			    System.out.println(
+				    "The first integer in -queries must be"
+					    + " greater than 0");
+			    print_usage_and_exit();
+			}
+			if (int1 > int2) {
+			    System.out.println(
+				    "The first integer in -queries must be"
+					    + " greater than the second");
+			    print_usage_and_exit();
+			}
+
+		    } else {
+			int1 = Integer.parseInt(args[i + 2]);
+			if (int1 < 0) {
+			    System.out.println(
+				    "The first integer in -queries must be"
+					    + " greater than 0");
+			    print_usage_and_exit();
+			}
 		    }
 		    i++;
 		}
-	    } else if ("-fieldsproc".equals(args[i])){
-		while (!args[i+1].contains("-")){
-		    fieldsproc[counter] = args[i+1];
-		    i++;
+	    } else if ("-fieldsproc".equals(args[i])) {
+		while (!args[i + 1].contains("-")) {
+		    if (args[i + 1].equals("T") || args[i + 1].equals("W")) {
+			fieldsproc[counter] = args[i + 1];
+			i++;
+		    } else {
+			System.out.println(
+				"The lista-campos in -fieldsproc item must be: T or W");
+			print_usage_and_exit();
+		    }
 		}
+		i++;
 		counter = 0;
-	    } else if ("-fieldsvisual".equals(args[i])){
-		while (!args[i+1].contains("-")){
-		    fieldsvisual[counter] = args[i+1];
-		    i++;
+	    } else if ("-fieldsvisual".equals(args[i])) {
+		while (!args[i + 1].contains("-")) {
+		    if (args[i + 1].equals("T") || args[i + 1].equals("I")
+			    || args[i + 1].equals("W")
+			    || args[i + 1].equals("B")
+			    || args[i + 1].equals("A")) {
+			fieldsproc[counter] = args[i + 1];
+			i++;
+		    } else {
+			System.out.println(
+				"The lista-campos item in -fieldsvisual must be: I,T,B,A or W");
+			print_usage_and_exit();
+		    }
 		}
+		i++;
 		counter = 0;
 	    }
 	}
     }
 
     private static void print_usage_and_exit() {
-	String usage = "Usage: Indexing options [-openmode openmode] [-index pathname] [-coll pathname] "
-		+ "[-indexingmodel default| jm lambda | dir mu] \n";
+	String usage = "Usage: Searching options [-search default|jm|dir] [-indexin pathname] [-cut n] "
+		+ "[-top n] [-queries all|int1|int1-int2] [fieldsproc lista-campos]"
+		+ " [fieldsvisual lista-campos] \n";
 	System.out.println(usage);
 	System.exit(0);
     }
